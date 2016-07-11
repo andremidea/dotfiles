bee9ea4093476385194ce2989cda4b96ed8aba03
André Midea <midea.andre@gmail.com>
2016-07-11 22:58:40 +0000

    adding new things

---
+++ qtile/config.py
@@ -0,0 +1,165 @@
+# Copyright (c) 2010 Aldo Cortesi
+# Copyright (c) 2010, 2014 dequis
+# Copyright (c) 2012 Randall Ma
+# Copyright (c) 2012-2014 Tycho Andersen
+# Copyright (c) 2012 Craig Barnes
+# Copyright (c) 2013 horsik
+# Copyright (c) 2013 Tao Sauvage
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from libqtile.config import Key, Screen, Group, Drag, Click, Match
+from libqtile.command import lazy
+from libqtile import layout, bar, widget
+
+mod = "mod4"
+
+keys = [
+    # Toggle between split and unsplit sides of stack.
+    # Split = all windows displayed
+    # Unsplit = 1 window displayed, like Max layout, but still with
+    # multiple stack panes
+    Key(
+        [mod, "shift"], "Return",
+        lazy.layout.toggle_split()
+    ),
+    Key([mod, "shift"], "Return", lazy.spawn("termite")),
+
+    # Toggle between different layouts as defined below
+    Key([mod], "space", lazy.next_layout()),
+    Key([mod, "shift"], "c", lazy.window.kill()),
+
+    Key([mod, "control"], "r", lazy.restart()),
+    Key([mod, "control"], "q", lazy.shutdown()),
+    Key([mod], "p", lazy.spawncmd()),
+    Key([mod], "w", lazy.to_screen(0)),
+    Key([mod], "r", lazy.to_screen(1)),
+    Key([mod], "e", lazy.to_screen(2)),
+
+    Key([mod], "h", lazy.layout.left()),
+    Key([mod], "l", lazy.layout.right()),
+    Key([mod], "j", lazy.layout.down()),
+    Key([mod], "k", lazy.layout.up()),
+    Key([mod, "shift"], "h", lazy.layout.swap_left()),
+    Key([mod, "shift"], "l", lazy.layout.swap_right()),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down()),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up()),
+    Key([mod], "i", lazy.layout.grow()),
+    Key([mod], "m", lazy.layout.shrink()),
+    Key([mod], "n", lazy.layout.normalize()),
+    Key([mod], "o", lazy.layout.maximize()),
+    Key([mod, "shift"], "space", lazy.layout.flip()),
+]
+
+groups = [Group("%s" % i) for i in range(1,9)]
+
+for i in groups:
+    # mod1 + letter of group = switch to group
+    keys.append(
+        Key([mod], i.name, lazy.group[i.name].toscreen())
+    )
+
+    # mod1 + shift + letter of group = switch to & move focused window to group
+    keys.append(
+        Key([mod, "shift"], i.name, lazy.window.togroup(i.name))
+    )
+
+groups = groups + [Group('steam', init=False, persist=False, layout='max',
+                         matches=[Match(wm_class=['Steam'])],
+                         position=9, exclusive=True,
+          )]
+
+layouts = [
+    layout.Max(),
+    layout.MonadTall()
+]
+
+widget_defaults = dict(
+    font='Arial',
+    fontsize=16,
+    padding=3,
+)
+
+screens = [Screen(top = bar.Bar([
+        # This is a list of our virtual desktops.
+        widget.GroupBox(urgent_alert_method='text', fontsize=11, this_current_screen_border='7b5830'),
+        widget.sep.Sep(foreground='7b5830'), #add separator bars where deemed necessary
+
+        # A prompt for spawning processes or switching groups. This will be
+        # invisible most of the time.
+        widget.Prompt(fontsize=10),
+        widget.Clipboard(timeout=10),
+        # Current window name.
+        widget.windowtabs.WindowTabs(),
+        widget.CurrentLayout(foreground='7b5830'),
+        widget.sep.Sep(foreground='7b5830'),
+        #NetworkStatus(theme_path='/home/deewakar/.config/qtile/icons/'),
+        widget.Volume(),
+        widget.sep.Sep(foreground='7b5830'),
+        widget.Systray(),
+        #display 12-hour clock
+        widget.Clock(format='%B %d %a %I:%M %p', fontsize=11, foreground='9c6b34'),
+    ], 22, opacity=0.1)) # our bar is 22px high
+]
+
+
+#screens = [
+#    Screen(
+#        bottom=bar.Bar(
+#            [
+#                widget.GroupBox(),
+#                widget.Prompt(),
+#                widget.WindowName(),
+#                widget.TextBox("default config", name="default"),
+#                widget.Systray(),
+#                widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
+#            ],
+#            30,
+#        ),
+#    ),
+#]
+
+# Drag floating layouts.
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(),
+        start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(),
+        start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front())
+]
+
+dgroups_key_binder = None
+dgroups_app_rules = []
+main = None
+follow_mouse_focus = True
+bring_front_click = False
+cursor_warp = True
+floating_layout = layout.Floating()
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+
+# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
+# string besides java UI toolkits; you can see several discussions on the
+# mailing lists, github issues, and other WM documentation that suggest setting
+# this string if your java app doesn't work correctly. We may as well just lie
+# and say that we're a working one by default.
+#
+# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
+# java that happens to be on java's whitelist.
+wmname = "LG3D"
